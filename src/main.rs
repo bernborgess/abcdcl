64202fb186b93ec3eb69511ee02350bdd6b35214
attiilla <atilappl@gmail.com>
2025-01-19 03:44:34 -0300

    run_demo commented

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use abcdclt::{cdcl::{run_cdcl, run_demo}, parser::read_cnf};
 
 fn main() {
-    //let (cnf, lits) = read_cnf();
+    let (cnf, lits) = read_cnf();
     // TODO: Pass the right number of literals!
-    //let result = run_cdcl(cnf, lits);
-    //println!("{:?}", result);
-    run_demo()
+    let result = run_cdcl(cnf, lits);
+    println!("{:?}", result);
+    //run_demo()
 }
