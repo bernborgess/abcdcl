6559c51b101c1685164d255244349bf037f00646
bernborgess <bernborgess@outlook.com>
2025-01-29 09:50:12 -0300

    Changed main for proof logging similar to minisat

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,19 @@ use abcdcl::{
 
 fn main() {
     let (cnf, lits) = read_cnf();
-    // TODO: Pass the right number of literals!
     let result = run_cdcl(&cnf, lits);
     match result {
-        CdclResult::SAT(_) => println!("\nSAT"),
+        CdclResult::SAT(model) => {
+            println!("SAT");
+            for (i, b) in model.iter().enumerate() {
+                if *b {
+                    print!("{i} ");
+                } else {
+                    print!("-{i} ");
+                }
+            }
+            println!();
+        }
         CdclResult::UNSAT => println!("\nUNSAT"),
     }
-    // eprintln!("{:?}", result);
-    //run_demo()
 }
