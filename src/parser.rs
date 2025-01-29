b443f6d652c427287dbd977a62599d7b01e29a47
bernborgess <bernborgess@outlook.com>
2025-01-29 09:49:49 -0300

    Clean up the parser code

--- src/parser.rs
+++ src/parser.rs
@@ -2,7 +2,28 @@ use dimacs::*;
 use std::env;
 use std::fs;
 
-// TODO: Return the datastructure we want to work with
+pub fn read_from_string(dimacs_filename: &str) -> (Vec<Vec<i64>>, usize) {
+    let contents = fs::read_to_string(dimacs_filename).expect("Couldn't find the DIMACS file ");
+    let (num_vars, clauses) = match parse_dimacs(&contents).unwrap() {
+        Instance::Cnf { num_vars, clauses } => (num_vars, clauses),
+        Instance::Sat { .. } => panic!("Received (valid) SAT input, not CNF"),
+    };
+    let mut cnf_vec: Vec<Vec<i64>> = vec![];
+    for clause in clauses.iter() {
+        let mut clause_vec = Vec::new();
+        for l in clause.lits().iter() {
+            let b = match l.sign() {
+                Sign::Neg => false,
+                Sign::Pos => true,
+            };
+            let val: i64 = l.var().to_u64() as i64;
+            clause_vec.push(if b { val } else { -val });
+        }
+        cnf_vec.push(clause_vec);
+    }
+    (cnf_vec, num_vars.try_into().unwrap())
+}
+
 pub fn read_cnf() -> (Vec<Vec<i64>>, usize) {
     let args: Vec<String> = env::args().collect();
     let dimacs_filename = args[1].as_str();
@@ -12,10 +33,6 @@ pub fn read_cnf() -> (Vec<Vec<i64>>, usize) {
         Instance::Cnf { num_vars, clauses } => (num_vars, clauses),
         Instance::Sat { .. } => panic!("Received (valid) SAT input, not CNF"),
     };
-    /*eprintln!(
-        "Working with {num_vars} variables and {} clauses.",
-        clauses.len()
-    );*/
     let mut cnf_vec: Vec<Vec<i64>> = vec![];
     for clause in clauses.iter() {
         let mut clause_vec = Vec::new();
@@ -24,43 +41,9 @@ pub fn read_cnf() -> (Vec<Vec<i64>>, usize) {
                 Sign::Neg => false,
                 Sign::Pos => true,
             };
-            //eprint!("{}{} ", if b { "+" } else { "-" }, l.var().to_u64());
             let val: i64 = l.var().to_u64() as i64;
-            // add this lit to the clause at the right idx
             clause_vec.push(if b { val } else { -val });
         }
-        //eprintln!();
-        cnf_vec.push(clause_vec);
-    }
-    (cnf_vec, num_vars.try_into().unwrap())
-}
-
-pub fn read_from_string(s: &str) -> (Vec<Vec<i64>>, usize) {
-    let dimacs_filename = s;
-    let contents = fs::read_to_string(dimacs_filename).expect("Couldn't find the DIMACS file ");
-
-    let (num_vars, clauses) = match parse_dimacs(&contents).unwrap() {
-        Instance::Cnf { num_vars, clauses } => (num_vars, clauses),
-        Instance::Sat { .. } => panic!("Received (valid) SAT input, not CNF"),
-    };
-    /*eprintln!(
-        "Working with {num_vars} variables and {} clauses.",
-        clauses.len()
-    );*/
-    let mut cnf_vec: Vec<Vec<i64>> = vec![];
-    for clause in clauses.iter() {
-        let mut clause_vec = Vec::new();
-        for l in clause.lits().iter() {
-            let b = match l.sign() {
-                Sign::Neg => false,
-                Sign::Pos => true,
-            };
-            //eprint!("{}{} ", if b { "+" } else { "-" }, l.var().to_u64());
-            let val: i64 = l.var().to_u64() as i64;
-            // add this lit to the clause at the right idx
-            clause_vec.push(if b { val } else { -val });
-        }
-        //eprintln!();
         cnf_vec.push(clause_vec);
     }
     (cnf_vec, num_vars.try_into().unwrap())
