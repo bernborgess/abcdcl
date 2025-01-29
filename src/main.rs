82ead8469e44e32ddda73b7b21684bb35148a9cc
bernborgess <bernborgess@outlook.com>
2025-01-29 14:15:44 -0300

    Add todo for the output, so we don't forget it

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,13 @@ use abcdcl::{
     cdcl::{run_cdcl, CdclResult},
     parser::read_cnf,
 };
-
+// TODO:
+// The output should be the SAT competition variation of the format.
+// Specifically, it should be a sequence of two lines. The first line should be
+// `s SATISFIABLE` or `s UNSATISFIABLE`. The next line should be blank unless the
+// first line is s SATISFIABLE. In that case, it should be v M , where M is a
+// space-separated list of non-contradictory literals, where each literal is just
+// a positive or negative (non-zero) integer.
 fn main() {
     let (cnf, lits) = read_cnf();
     let result = run_cdcl(&cnf, lits);
